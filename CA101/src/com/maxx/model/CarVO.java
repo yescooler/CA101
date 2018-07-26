8b7ab84bc0ff4052b3640bde60edba739cfe14c8
sekatsumoto <sekatsumoto@172.20.10.3>
2018-07-26 17:07:13 +0800

    try 1

---
+++ CA101/src/com/maxx/model/CarVO.java
@@ -0,0 +1,21 @@
+package com.maxx.model;
+
+public class CarVO {
+	private String brand;
+	private Integer price;
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	 
+	
+
+}
